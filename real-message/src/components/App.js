c354b089ab274abdfbf3213bc23b962a7f590631
Bob <rellrocker235@gmail.com>
2021-01-18 16:15:55 +0200

    Local Storage
    Creation of the Local Storage to Store ID's so that they can be used to differentiate users in the application

--- real-message/src/components/App.js
+++ real-message/src/components/App.js
@@ -1,17 +1,19 @@
-import React, {useState} from 'react'
+import React from 'react'
+import useLocalStorage from '../hooks/useLocalStorage';
 import Login from './Login'
 
 function App() {
 
   // stores all the state for the Id that is shared all accross the application
-  const [id, setId] = useState()
+  const [id, setId] = useLocalStorage()
 
-  return (
-      
-        <Login onIdSubmit={setId}/>
-      
+  return(
+        <>
+          {id}
+          <Login onIdSubmit={setId}/>
+        </>
   )
 }
 
-export default App
+export default App;
 
