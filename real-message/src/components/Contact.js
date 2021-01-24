8c97b339a7d0cd9a014de0b29c2d8420138e1c1e
Bob <rellrocker235@gmail.com>
2021-01-24 16:13:04 +0200

    Conversation Modal and Provider Part 1
    Set up up the Conversation Modal and Provider that Play a Similar Role to the Ones of the Contacts

--- real-message/src/components/Contact.js
+++ real-message/src/components/Contact.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import {useContacts} from '../context/ContactsProvider'
+import {ListGroup} from 'react-bootstrap'
 
 function Contact() {
+    // Were going to make it possible for our contacts to be Displayed on the Screen
+    const {contacts} = useContacts() // Gives us access to our Contacts
+
+
     return (
-        <div>
-            Would You Like to Add a Contact
-        </div>
+        <ListGroup variant="flush">
+            {contacts.map(contact =>(
+                <ListGroup.Item key={contact.id}>  {/*Prevention of Duplicate ID's*/}
+                    {contact.name}
+                </ListGroup.Item>
+            ))}
+
+        </ListGroup>
     )
 }
 
---
+++ real-message/src/context/ConversationProvider.js
@@ -0,0 +1,34 @@
+import React,{useContext} from 'react'
+import useLocalStorage from '../hooks/useLocalStorage'
+
+const ConversationsContext = React.createContext()
+
+export function useConversations(){
+    //This is a Quick Shorthand so that we can use our Contacts
+    //Essetnially usecontext is for passing down props withouth having to manully implementing them
+    return useContext(ConversationsContext)
+}
+
+export function ConversationsProvider({children}) {
+
+    const[conversations,SetConversations] = useLocalStorage('conversations',[]) // This will be the Array that all the contact information will be stored in
+
+    //this fucntion below deals specifically with creating our contacts
+    function createConversation(recipients){
+        SetConversations(prevConversations =>{
+            return[...prevConversations,{recipients, message : []}]
+        })
+    }
+
+    return (
+        //The Provider is used to wrap around any code that needs access to the infomation in the context as shown in the App.js File
+        // It has a Single Prop called value which represent what the context is.
+        //Via the Provider we have access to the createContact function
+        <ConversationContext.Provider value={{conversations, createConversation}}>
+            {children}
+        </ConversationContext.Provider>
+    )
+}
+
+
+
