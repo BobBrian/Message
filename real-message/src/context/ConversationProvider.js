26dc7e86b66dcaddc613a018121d50c9a8226efc
Bob <rellrocker235@gmail.com>
2021-01-29 21:40:39 +0200

    Creation of Actual Conversation Part 1
    Creation fo the Formated Conversations which essentially
    there to show the Information of Users Having Conversations with each other in order to better identify them

--- real-message/src/context/ConversationProvider.js
+++ real-message/src/context/ConversationProvider.js
@@ -1,5 +1,6 @@
 import React,{useContext} from 'react'
 import useLocalStorage from '../hooks/useLocalStorage'
+import { useContacts } from './ContactsProvider'
 
 const ConversationsContext = React.createContext()
 
@@ -14,6 +15,8 @@ export function ConversationsProvider({children}) {
     const[conversations,SetConversations] = useLocalStorage('conversations',[]) // This will be the Array that all the conversation
     // information will be stored in
 
+    const { contacts } = useContacts()
+
     //this fucntion below deals specifically with creating our contacts
     function createConversation(recipients){
         SetConversations(prevConversations =>{
@@ -21,11 +24,33 @@ export function ConversationsProvider({children}) {
         })
     }
 
+    const formattedConversations = conversations.map(conversations =>{
+        // This is to show and Display the Information of those we are having a Conversation with
+        const recipients = conversations.recipients.map(recipient => {
+            const contact = contacts.find(contact => {
+                // This is to check whether or not an enterd contact exist on not when searching through a list of them
+                // And if it does then it is displayed , but this is purely for the ID
+                return contact.id === recipient
+            })
+            const name = (contact && contact.name) || recipient
+            // This is for  Physically Displaying the Name of the Searched Contact , if it has a Name Associated with the ID , then 
+            // That to will be Displayed but if not then just the ID number , Think How in Whatsapp if you add a Someone there
+            // but don't have them as a Contact then only there Phone Number will be Displayed.
+            return { id: recipient, name }
+        })
+        return {...conversations, recipients}
+    })
+
+    const value = {
+        conversations: formattedConversations,
+        createConversation
+    }
+
     return (
         //The Provider is used to wrap around any code that needs access to the infomation in the context as shown in the App.js File
         // It has a Single Prop called value which represent what the context is.
         //Via the Provider we have access to the createContact function
-        <ConversationsContext.Provider value={{conversations, createConversation}}>
+        <ConversationsContext.Provider value={value}>
             {children}
         </ConversationsContext.Provider>
     )
